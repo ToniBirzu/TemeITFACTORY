39d6f4d18fc65f907f161d611df1747ca16bc6af
ToniBirzu <125299168+ToniBirzu@users.noreply.github.com>
2023-02-14 17:49:08 +0200

    Add files via upload

---
+++ Tema.py
@@ -0,0 +1,40 @@
+#Exercitiul 2
+#variabila de tip string in Python reprezinta un sir de caractere
+#care se scrie intre ghilimele simple.
+
+#Exercitiul 4
+# String
+prenume = 'Toni'
+print(prenume)
+# Intiger
+varsta = 28
+print(varsta)
+# float
+numar = 2.537
+print(numar)
+# boolean
+rosu = True
+print(True)
+#rezultat Exercitiul 4
+#Output:
+
+#Toni
+#28
+#2.537
+#True
+
+#Exercitiul 6
+i = 1
+while i < 20:
+    print(i)
+    i = i + 3
+#rezultat Exercitiul 6
+#Output:
+
+#1
+#4
+#7
+#10
+#13
+#16
+#19
